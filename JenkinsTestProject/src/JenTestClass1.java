3243ca4926f267784685e78b5a21cca460e4d938
Hvuser <Hvuser@EBHVR15-02-05.hexa.training.local>
2018-10-16 16:35:18 +0530

    firt commit in github

---
+++ JenkinsTestProject/src/JenTestClass1.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Hvuser
+ *
+ */
+public class JenTestClass1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Jen - Git Hub Project");
+	}
+
+}
